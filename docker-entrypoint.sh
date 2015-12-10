fdcfed9b10660c102b1a088539db39763ebd72b5
Mikhail Merkulov <Mihail.Merkulov@gmail.com>
2015-12-10 19:29:29 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "Applying token" 
 echo $TOKEN
 
-exec sed -i "s#\$TOKEN#$TOKEN#" /fluentd/etc/$FLUENTD_CONF
+sed -i "s#\$TOKEN#$TOKEN#" /fluentd/etc/$FLUENTD_CONF
 
 echo "Running fluentd"
 exec fluentd -v -c /fluentd/etc/$FLUENTD_CONF -p /fluentd/plugins $FLUENTD_OP
